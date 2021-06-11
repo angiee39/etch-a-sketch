0cd43b9b1ffd8c2131a5cadf69138c0985ca399d
angiee39 <anjalarox@gmail.com>
2021-06-11 18:32:04 +0530

    Added prompt user for grid size, clears grid each promp, need to set a fixed size for the grid.

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-// Make 16x16 grid
-for (i = 0; i < 16; i++) {
-    const grid = document.querySelector('#grid');
-    const div = document.createElement('div');
-    grid.appendChild(div);
-    div.setAttribute('id', 'row');
-    div.style.display = 'flex';
+
+// Make grid
+function makeGrid(input) {
+
+    for (i = 0; i < input; i++) {
+        const grid = document.querySelector('#grid');
+        const div = document.createElement('div');
+        grid.appendChild(div);
+        div.setAttribute('id', 'row');
+        div.style.display = 'flex';
+    }
+    for (j = 0; j < input; j++) {
+        const rows = document.querySelectorAll('#row')
+        rows.forEach((row) => {
+            const column = document.createElement('div');
+            row.appendChild(column);
+            column.setAttribute('id', 'column');
+            column.addEventListener('mouseout', function (e) {
+                column.style.backgroundColor = "cornsilk"
+            });
+        });  
+    }
+};
+
+// Clear grid
+function clearGrid() {
+    const grid2 = document.querySelector('#grid')
+    while (grid2.firstChild) {
+        grid2.removeChild(grid2.firstChild);
+    }
+};
+
+
+
+
+
+// User input
+function userIn() {
+    
+    while (true)
+    {
+        let userInput = window.prompt("Enter grid size:");
+        let uInput = parseInt(userInput);
+        if (uInput >= 1 || uInput <= 64)
+        {
+            makeGrid(uInput)
+            break;
+        }
+    }
+    
 }
 
-for (j = 0; j < 16; j++) {
-    const rows = document.querySelectorAll('#row')
-    rows.forEach((row) => {
-        const column = document.createElement('div');
-        row.appendChild(column);
-        column.setAttribute('id', 'column');
-    });  
-}
+// Refresh
+btn.addEventListener('click', function (e) {
+    clearGrid();
+    userIn();
+});
+
+makeGrid(16)
 
